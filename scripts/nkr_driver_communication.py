0fdc12a55de027d4de2e11ea525968372d550055
Konstantin <pec-orange@mail.ru>
2021-11-22 12:43:59 +0300

    [debug] nkr_driver debug

--- scripts/nkr_driver_communication.py
+++ scripts/nkr_driver_communication.py
@@ -13,40 +13,48 @@ ser = serial.Serial(
 	parity = serial.PARITY_NONE,
 	stopbits = serial.STOPBITS_ONE,
 	bytesize = serial.EIGHTBITS,
-	timeout = 0.1 )
+	timeout = 0.1)
 
 anr1 = 0
 anr2 = 0
 anr3 = 0
 anr4 = 0
 turn = 0
-
+calibration_needed = False
 stop = 0
 
 def keyboard_callback(data):
-    global anr1, anr2, anr3, anr4, turn, stop
+    global anr1, anr2, anr3, anr4, turn, stop, calibration_needed
     if data.data=="f":
         if anr1 <100:
+            stop = 0
             anr1 = anr1+5
             anr2 = anr2+5
             anr3 = anr3+5
             anr4 = anr4+5
     if data.data=="b":
         if anr1>-100:
+            stop = 0
             anr1 = anr1-5
             anr2 = anr2-5
             anr3 = anr3-5
             anr4 = anr4-5
     if data.data=="l":
-        anr1 = anr1-5
-        anr2 = anr2+5
-        anr3 = anr3-5
-        anr4 = anr4+5
+        turn =  turn - 5
+        if turn  < -30:
+            turn = -30
+        #anr1 = anr1-5
+        #anr2 = anr2+5
+        #anr3 = anr3-5
+        #anr4 = anr4+5
     if data.data=="r":
-        anr1 = anr1+5
-        anr2 = anr2-5
-        anr3 = anr3+5
-        anr4 = anr4-5
+        turn =  turn + 5
+        if turn > 30:
+            turn = 30
+        #anr1 = anr1+5
+        #anr2 = anr2-5
+        #anr3 = anr3+5
+        #anr4 = anr4-5
 
     if data.data=="s":
         stop = 1
@@ -54,22 +62,36 @@ def keyboard_callback(data):
         anr2 = 0
         anr3 = 0
         anr4 = 0
+    if data.data=="o": # calibration command
+        stop = 1
+        turn = 0
+        anr1 = 0
+        anr2 = 0
+        anr3 = 0
+        anr4 = 0
+        calibration_needed = True
 
 
 
 
 def communicator():
-    global anr1, anr2, anr3, anr4, turn, stop
+    global anr1, anr2, anr3, anr4, turn, stop, calibration_needed
     rospy.init_node('nkr_driver_communication', anonymous=True)
     rospy.Subscriber('keyboard_commands', String, keyboard_callback)
-    rate = rospy.Rate(10)
+    rate = rospy.Rate(5)
 
     while not rospy.is_shutdown():
-
         ser.write("[drv] {} {} {} {} {} {}\n".format(anr1, anr2, anr3, anr4, turn, int(stop)))
-        print('[drv] {} {} {} {} {} {}\n'.format(anr1, anr2, anr3, anr4, turn, int(stop)))
+        #print('[drv] {} {} {} {} {} {}\n'.format(anr1, anr2, anr3, anr4, turn, int(stop)))
+        #print(ser.readline())
         print(ser.readline())
-        print(ser.readline())
+
+        if calibration_needed:
+            print('sending rotation angles calibration command\n')
+            time.sleep(1)
+            ser.write("[cal] \n")
+
+        rate.sleep()
 
 
 if __name__ == '__main__':
